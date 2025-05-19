f5d816d192ed7fb3f4a945cc6cf3542df207601a
pramosiz <ramosizquierdop@gmail.com>
2025-05-19 14:09:55 +0200

    Official Kafka image

--- resources/postgres.sh
+++
@@ -1,8 +0,0 @@
-docker run \
- -d --name postgres \
- -p 5433:5432 \
- -e POSTGRES_DB=test \
- -e POSTGRES_USER=postgres \
- -e POSTGRES_PASSWORD=postgres \
- -v /Users/pabloramosizquierdo/Desktop/Pablo/Tecnologias/Dockers/PostgreSQL/data:/var/lib/postgresql/data \
- postgres:14.13
